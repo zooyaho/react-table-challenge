b94b7e1c31af812d18e56e62bfd1d5a1d6044543
zooyaho <jiwoo1268@naver.com>
2025-01-15 22:20:36 +0900

    feat: tanstack query설치 및 provider설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import AppRoutes from "@/routes/AppRoutes";
+import Providers from "@/providers";
 
 function App() {
   return (
-    <>
+    <Providers>
       <AppRoutes />
-    </>
+    </Providers>
   );
 }
 
