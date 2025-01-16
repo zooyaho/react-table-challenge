333b13c8b780ad061a5e71a2616acbcc7a695282
zooyaho <jiwoo1268@naver.com>
2025-01-17 03:52:27 +0900

    remove: recoil 미사용으로 관련 파일 삭제

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,11 +1,6 @@
 import { PropsWithChildren } from "react";
 import TanstackQueryProvider from "@/providers/TanstackQueryProvider";
-import { RecoilRoot } from "recoil";
 
 export default function Providers({ children }: PropsWithChildren) {
-  return (
-    <RecoilRoot>
-      <TanstackQueryProvider>{children}</TanstackQueryProvider>
-    </RecoilRoot>
-  );
+  return <TanstackQueryProvider>{children}</TanstackQueryProvider>;
 }
